1fecd002010de51ff812fc19ac6b70ab4cfb2b88
Pratham Shah <ppspr@pop-os.localdomain>
2022-02-27 12:57:01 +0530

    added dependencies for spectrahedra

---
+++ examples/optimization_spectrahedra/external/arpack-ng-build/CTestTestfile.cmake
@@ -0,0 +1,18 @@
+# CMake generated Testfile for 
+# Source directory: /home/ppspr/volesti/examples/arpackpp/external/arpack-ng
+# Build directory: /home/ppspr/volesti/examples/arpackpp/external/arpack-ng-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(dnsimp_tst "/home/ppspr/volesti/examples/arpackpp/external/arpack-ng-build/TESTS/dnsimp")
+set_tests_properties(dnsimp_tst PROPERTIES  _BACKTRACE_TRIPLES "/home/ppspr/volesti/examples/arpackpp/external/arpack-ng/CMakeLists.txt;593;add_test;/home/ppspr/volesti/examples/arpackpp/external/arpack-ng/CMakeLists.txt;0;")
+add_test(bug_1323_tst "/home/ppspr/volesti/examples/arpackpp/external/arpack-ng-build/TESTS/bug_1323")
+set_tests_properties(bug_1323_tst PROPERTIES  _BACKTRACE_TRIPLES "/home/ppspr/volesti/examples/arpackpp/external/arpack-ng/CMakeLists.txt;609;add_test;/home/ppspr/volesti/examples/arpackpp/external/arpack-ng/CMakeLists.txt;0;")
+add_test(bug_58_double_tst "/home/ppspr/volesti/examples/arpackpp/external/arpack-ng-build/TESTS/bug_58_double")
+set_tests_properties(bug_58_double_tst PROPERTIES  _BACKTRACE_TRIPLES "/home/ppspr/volesti/examples/arpackpp/external/arpack-ng/CMakeLists.txt;613;add_test;/home/ppspr/volesti/examples/arpackpp/external/arpack-ng/CMakeLists.txt;0;")
+add_test(bug_79_double_complex_tst "/home/ppspr/volesti/examples/arpackpp/external/arpack-ng-build/TESTS/bug_79_double_complex")
+set_tests_properties(bug_79_double_complex_tst PROPERTIES  _BACKTRACE_TRIPLES "/home/ppspr/volesti/examples/arpackpp/external/arpack-ng/CMakeLists.txt;617;add_test;/home/ppspr/volesti/examples/arpackpp/external/arpack-ng/CMakeLists.txt;0;")
+add_test(bug_142_tst "/home/ppspr/volesti/examples/arpackpp/external/arpack-ng-build/TESTS/bug_142")
+set_tests_properties(bug_142_tst PROPERTIES  _BACKTRACE_TRIPLES "/home/ppspr/volesti/examples/arpackpp/external/arpack-ng/CMakeLists.txt;621;add_test;/home/ppspr/volesti/examples/arpackpp/external/arpack-ng/CMakeLists.txt;0;")
+add_test(bug_142_gen_tst "/home/ppspr/volesti/examples/arpackpp/external/arpack-ng-build/TESTS/bug_142_gen")
+set_tests_properties(bug_142_gen_tst PROPERTIES  _BACKTRACE_TRIPLES "/home/ppspr/volesti/examples/arpackpp/external/arpack-ng/CMakeLists.txt;625;add_test;/home/ppspr/volesti/examples/arpackpp/external/arpack-ng/CMakeLists.txt;0;")
